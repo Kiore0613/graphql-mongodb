3421d8daa183f423515820b68548368621b5875c
kiore <ricardochicas95@gmail.com>
2021-04-25 13:44:16 -0600

    Lesson Type

---
+++ src/lesson/lesson.type.ts
@@ -0,0 +1,17 @@
+import { Field, ObjectType, ID } from "@nestjs/graphql";
+
+@ObjectType('Lesson')
+export class lessonType {
+
+  @Field(type => ID)
+  id: string
+
+  @Field()
+  name: string
+
+  @Field()
+  startDate: string
+
+  @Field()
+  endDate: string
+}
