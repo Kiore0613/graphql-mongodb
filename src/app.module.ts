cfc582bb4fdefb98faf93bed212d223290ffcc16
kiore <ricardochicas95@gmail.com>
2021-04-25 14:21:38 -0600

    mongodb connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
-import { Module } from '@nestjs/common';
+import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
 import { GraphQLModule } from '@nestjs/graphql'
-import { LessonModule } from './lesson/lesson.module';
+import { LessonModule } from './lesson/lesson.module'
 
 @Module({
-  imports: [GraphQLModule.forRoot({
-    autoSchemaFile: true
-  }), LessonModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: 'mongodb:172.17.0.2:27017/school',
+      synchronize: true,
+      useUnifiedTopology: true,
+      entities: []
+    }),
+    GraphQLModule.forRoot({
+      autoSchemaFile: true
+    }), LessonModule],
 })
 export class AppModule { }
